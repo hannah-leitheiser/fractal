94968708c63a48ae06d9acbd74fe9077beef1239
hannah-leitheiser <leitheiserhannah@runbox.com>
2018-02-04 21:50:17 +0000

    Commit.

--- fractal_pil.py
+++ fractal_pil.py
@@ -13,7 +13,8 @@ imageSize = (1000,800)
 
 def Mandlebrot(x, y, maxIterations=31):
 	"""Returns 0 if the point x + iy is in the Mandlebrot set 
-	or the number of iterations of f(z) = z^2 + c before exiting bounds."""
+	   or the number of iterations of f(z) = z^2 + c 
+	   before exiting bounds."""
 	(c, z) = (complex(x, y), 0)
 	for i in range(maxIterations):
 		z = z**2 + c
@@ -27,7 +28,8 @@ pixels = image.load()
 for x in range(imageSize[0]):
 	for y in range(imageSize[1]):
 		# This involved some trial and error to center the graph.
-		cordx = (x - imageSize[0]/2 - 0.3*imageSize[1]) / (imageSize[1]*0.5)
+		cordx = (x - imageSize[0]/2 - 0.3*imageSize[1]) 
+		             / (imageSize[1]*0.5)
 		cordy = (y - imageSize[1]/2) / (imageSize[1]*0.5)
 		# We'll just vary the red.
 		pixels[x,y] = (Mandlebrot(cordx, cordy)*8,0,0)
